7ff418e9b661e26d2af9b54bf615f91f66487c49
BENOITMASSC3F4A\Benoit <benoit.masschelein@hotmail.com>
2018-04-03 16:22:05 +0200

    Certifcate Authority toegevoegd

---
+++ workspace/CertificateAuthority/src/CAService.java
@@ -0,0 +1,62 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class CAService {
+	public static void main(String[] args) {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+
+            // Initialize KeyPairGenerator.
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            keyGen.initialize(1024, random);
+
+            // Generate Key Pairs, a private key and a public key.
+            KeyPair keyPair = keyGen.generateKeyPair();
+            PrivateKey privateKey = keyPair.getPrivate();
+            PublicKey publicKey = keyPair.getPublic();
+
+            Base64.Encoder encoder = Base64.getEncoder();
+            System.out.println("privateKey: " + encoder.encodeToString(privateKey.getEncoded()));
+            System.out.println("publicKey: " + encoder.encodeToString(publicKey.getEncoded()));
+            
+            saveKeyPair("Keys/", keyPair);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+	
+	private static void saveKeyPair(String path, KeyPair keyPair) throws IOException {
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+ 
+		// Store Public Key.
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+				publicKey.getEncoded());
+		FileOutputStream fos = new FileOutputStream(path + "/public.key");
+		fos.write(x509EncodedKeySpec.getEncoded());
+		fos.close();
+ 
+		// Store Private Key.
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+				privateKey.getEncoded());
+		fos = new FileOutputStream(path + "/private.key");
+		fos.write(pkcs8EncodedKeySpec.getEncoded());
+		fos.close();
+	}
+
+}
